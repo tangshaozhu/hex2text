0a8aef3621b9b70a442b9094ba2e9adedcd86098
tangshaozhu <tsz.aqu@qq.com>
2023-01-13 01:00:53 +0800

    支持有符号数（仅十进制） demo支持导出到文件

--- demo.py
+++ demo.py
@@ -2,5 +2,12 @@ import sys
 from hex2text import get_data_from_hex
 
 data = get_data_from_hex('demo.txt', 2, 10)
-for i, d in enumerate(data):
-    print(i, d)
+
+if len(sys.argv) == 1: 
+    for d in data:
+        print(d)
+else:
+    with open(sys.argv[1], 'w') as f:
+        for d in data:
+            f.write(d)
+            f.write('\n')
--- hex2text.py
+++ hex2text.py
@@ -1,4 +1,4 @@
-def _get_num_str(hexstr, width, base=10):
+def _get_num_str(hexstr:str, width:int, base:int, signed:bool):
     if base not in [2, 8, 10, 16]:
         raise Exception("width value must be 1, 2, 4 or 8")
     bytes = []
@@ -14,12 +14,15 @@ def _get_num_str(hexstr, width, base=10):
     if base == 8:
         return oct(num)
     if base == 10:
+        if signed:
+            return str(num - (1 << (width * 8)))
         return str(num)
 
 
-def get_data_from_hex(file, width, base):
-    if width not in [1, 2, 4, 8]:
-        raise Exception("width value must be 1, 2, 4 or 8")
+def get_data_from_hex(file, width, base=10, signed=False):
+    '''
+    仅用于处理keil save命令导出内存生成hex文件，返回相应格式数据字符串列表
+    '''
     data = []
     with open(file, 'r') as f:
         lines = f.readlines()
@@ -29,6 +32,6 @@ def get_data_from_hex(file, width, base):
                 typesize = datasize // width
                 offset = 9
                 for j in range(typesize):
-                    data.append(_get_num_str(line[offset:], width, base))
+                    data.append(_get_num_str(line[offset:], width, base, signed))
                     offset = offset + width * 2
     return data
